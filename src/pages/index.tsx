881a575810e778676dd72abe2b9011492cb7acec
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-15 03:58:03 +0900

    トップページ(文字の配置)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,5 @@
-import { Box,Text } from "@chakra-ui/react";
+import { TopPage } from "./TopPage";
 
 export default function Home() {
-  return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-    >
-      <Text textAlign="center" fontSize="50" fontWeight="700" textDecoration="underline">
-        Next.jsでChakraUIの練習
-      </Text>
-    </Box>
-  );
+  return <TopPage/>
 }
