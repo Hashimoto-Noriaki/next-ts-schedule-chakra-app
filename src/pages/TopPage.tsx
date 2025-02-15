23f39edb8a75645be7f9cd966baf54546be55e0c
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-16 02:19:13 +0900

    トップページの作成3

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,22 +1,41 @@
-import { Box, Flex, HStack, Link, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, HStack, Link, Text } from "@chakra-ui/react";
 
 export const TopPage = () => {
   return (
-    <Box>
-        <Flex as="header" p="4">
-            <Text fontSize="xl" fontWeight="bold">
-                スケジュール管理App
+    <Box position="relative">
+        <Box 
+            as="header"
+            position="fixed"
+            right="0"
+            left="0"
+            top="0"
+            py="4"
+        >
+            <Flex
+                justify="space-between"
+                px="6"
+            >
+                {/* ロゴ */}
+                <Text fontSize="xl" fontWeight="bold"className="logo" >
+                    スケジュール管理App
+                </Text>
+                {/* ナビゲーション */}
+                <HStack spacing="6" pr="6">
+                    <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
+                        利用方法
+                    </Link>
+                    <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
+                        ログイン
+                    </Link>
+                </HStack>
+            </Flex>
+        </Box>
+        {/* コンテンツエリア */}
+        <Box textAlign="center" pt="80px">
+            <Text fontWeight="bold" fontSize="30">
+                ようこそ！スケジュール管理APPへ
             </Text>
-            <Spacer />
-            <HStack spacing="6">
-                <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
-                    利用方法
-                </Link>
-                <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
-                    ログイン
-                </Link>
-            </HStack>
-        </Flex>
+        </Box>
     </Box>
   )
 }
