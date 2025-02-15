80b69f82b61903f39967d70110e4285da1bb2521
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-15 13:00:55 +0900

    トップページの作成3(練習)

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,25 +1,45 @@
-import { Box, Flex, Spacer, Text, HStack, Link } from "@chakra-ui/react";
+import { Box, Flex, HStack, Link, Text } from "@chakra-ui/react";
 
 export const TopPage = () => {
   return (
-    <Box>
+    <Box position="relative">
       {/* ヘッダー */}
-      <Flex as="header" p="4" align="center">
-        {/* 左側のロゴ */}
-        <Text fontSize="xl" fontWeight="bold">
-          スケジュール管理APP
+      <Box
+        as="header"
+        position="fixed"
+        top="0"
+        left="0"
+        right="0"
+        py="4"
+      >
+        <Flex
+          px="6"
+          alignItems="center"
+          justify="space-between"
+        >
+          {/* ロゴ */}
+          <Text fontSize="xl" fontWeight="bold" className="logo">
+            スケジュール管理APP
+          </Text>
+
+          {/* ナビゲーション（右寄せ） */}
+          <HStack spacing="6" color="lime.800" pr="6">
+            <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
+              利用方法
+            </Link>
+            <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
+              ログイン
+            </Link>
+          </HStack>
+        </Flex>
+      </Box>
+
+      {/* コンテンツエリア (ヘッダーの下に適切な余白を追加) */}
+      <Box pt="80px" textAlign="center">
+        <Text fontSize="2xl" fontWeight="bold">
+          ようこそ！スケジュール管理APPへ
         </Text>
-        <Spacer />
-        {/* 右側のナビゲーション */}
-        <HStack spacing="6">
-          <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
-            ご利用方法
-          </Link>
-          <Link href="#" fontSize="md" _hover={{ textDecoration: "underline" }}>
-            ログイン
-          </Link>
-        </HStack>
-      </Flex>
+      </Box>
     </Box>
   );
 };
