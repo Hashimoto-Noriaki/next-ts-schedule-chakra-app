22a3e622850a1de4740966cfdac0ae4dc16c4504
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-24 02:45:26 +0900

    ChakaUIをインストールして画面表示

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
